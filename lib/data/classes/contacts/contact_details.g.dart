81ef9bc520b10a1ca60d53105d1cc9c932d7ba9f
Rody Davis <rdavis@unifycrm.com>
2018-12-14 14:39:27 -0500

    Update contact_details.g.dart

--- lib/data/classes/contacts/contact_details.g.dart
+++ lib/data/classes/contacts/contact_details.g.dart
@@ -13,7 +13,32 @@ ContactDetails _$ContactDetailsFromJson(Map<String, dynamic> json) {
       email: json['Email_Address'] as String,
       address: json['Address'] == null
           ? null
-          : Address.fromJson(json['Address'] as Map<String, dynamic>));
+          : Address.fromJson(json['Address'] as Map<String, dynamic>),
+      homePhone: json['Home_Phone'] == null
+          ? null
+          : Phone.fromJson(json['Home_Phone'] as Map<String, dynamic>),
+      cellPhone: json['Cell_Phone'] == null
+          ? null
+          : Phone.fromJson(json['Cell_Phone'] as Map<String, dynamic>),
+      officePhone: json['Office_Phone'] == null
+          ? null
+          : Phone.fromJson(json['Office_Phone'] as Map<String, dynamic>),
+      companyCategory: json['Company_Category'] == null
+          ? null
+          : CompanyCategory.fromJson(
+              json['Company_Category'] as Map<String, dynamic>),
+      contactGroups: (json['Contact_Groups'] as List)
+          ?.map((e) => e == null
+              ? null
+              : ContactGroup.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      faxPhone: json['Fax_Phone'] == null
+          ? null
+          : Phone.fromJson(json['Fax_Phone'] as Map<String, dynamic>),
+      secondCellPhone: json['Secondary_Cell_Phone'] == null
+          ? null
+          : Phone.fromJson(
+              json['Secondary_Cell_Phone'] as Map<String, dynamic>));
 }
 
 Map<String, dynamic> _$ContactDetailsToJson(ContactDetails instance) =>
@@ -21,5 +46,12 @@ Map<String, dynamic> _$ContactDetailsToJson(ContactDetails instance) =>
       'First_Name': instance.firstName,
       'Last_Name': instance.lastName,
       'Email_Address': instance.email,
-      'Address': instance.address
+      'Address': instance.address,
+      'Cell_Phone': instance.cellPhone,
+      'Office_Phone': instance.officePhone,
+      'Home_Phone': instance.homePhone,
+      'Fax_Phone': instance.faxPhone,
+      'Secondary_Cell_Phone': instance.secondCellPhone,
+      'Company_Category': instance.companyCategory,
+      'Contact_Groups': instance.contactGroups
     };
